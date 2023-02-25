163c1b19c441bf3e0610bb0f94c022a9e7d13995
Đỗ Hữu Đức <huuduc0705@gmail.com>
2023-02-25 13:21:43 +0700

    Cấu hình Layout

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+// Lay out
+import { HeaderOnly } from "~/Components/Layout";
+//Pages
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Home copy";
+
+//Public Routes
+const publicRoutes = [
+    {path: '/', component: Home },
+    {path: '/Following', component: Following },
+    {path: '/Profile', component: Profile },
+    {path: '/Upload', component: Upload, layout: HeaderOnly },
+    {path: '/Search', component: Search, layout: null },
+
+
+]
+
+const privateRoutes = [
+    
+]
+
+export {publicRoutes, privateRoutes}
